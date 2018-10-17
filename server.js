ce498496bd3cb1648b34021692567f8a0c7ed052
felix <felix.kock.98@gmail.com>
2018-10-17 13:01:56 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+
+//================================================================//
+
+//Code übernommen von Prof Dr. Plaß, HAW Hamburg
+
+// Datenbank initialisieren
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('user.db');
+
+// Express.js Webserver
+const express = require('express');
+const app = express()
+
+// Body-Parser: wertet POST-Formulare aus
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// EJS Template Engine
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+
+// Sessionvariablen
+const session = require('express-session');
+app.use(session({ 
+	secret: 'example',
+	resave: false,
+	saveUninitialized: true
+}));
+
+// Passwort Verschlüsselung
+const passwordHash = require('password-hash');
+
+// Webserver starten http://localhost:3000
+app.listen(3000, function(){
+	console.log("listening on 3000");
+});
+
+// ================================================================//
