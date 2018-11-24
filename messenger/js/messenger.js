690402880e4648b20964733b5cec180f41af6bc0
Niklas <niklas@Celines-MacBook-Pro.local>
2018-11-24 14:19:55 +0100

    Samstag 24.11.2018 style

--- messenger/js/messenger.js
+++ messenger/js/messenger.js
@@ -1,12 +1,3 @@
-function changeText() {
-  document.getElementById("demo").innerHTML = "Hello JavaScript!";
-  document.getElementById("heading").style["color"] = "red";
-}
-
-function hide() {
-  document.getElementById("demo").style.display = "none";
-}
-
-function show() {
-  document.getElementById("demo").style.display = "block";
-}
+$( "#mailSenden" ).click(function() {
+  alert("Mail wurde gesendet!");
+});
